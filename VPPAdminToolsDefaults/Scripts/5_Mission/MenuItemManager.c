cc1adfc68efd6d0059dd27b4e8f67f8f98300b00
Z User <z@container>
2026-08-03 19:06:46 +0000

    fix: simplify OnCreate override, remove dropdown text hack

--- VPPAdminToolsDefaults/Scripts/5_Mission/MenuItemManager.c
+++ VPPAdminToolsDefaults/Scripts/5_Mission/MenuItemManager.c
@@ -29,27 +29,6 @@ modded class MenuItemManager
 		}
 
 		// === Placement dropdown: On Ground ===
-		Widget panel = M_SUB_WIDGET.FindAnyWidget("PlacementDropDownPanel");
-		if (panel)
-		{
-			// VPPDropDownMenu creates its root as first child of the panel
-			Widget ddRoot = panel.GetChildren();
-			if (ddRoot)
-			{
-				TextWidget ddText = TextWidget.Cast(ddRoot.FindAnyWidget("dropdown_text"));
-				if (ddText)
-				{
-					ddText.SetText("#VSTR_LBL_ON_GROUND");
-					Print("[VPPDefaults] Dropdown text -> #VSTR_LBL_ON_GROUND");
-				} else {
-					Print("[VPPDefaults] ERROR: dropdown_text not found");
-				}
-			}
-		} else {
-			Print("[VPPDefaults] ERROR: PlacementDropDownPanel not found");
-		}
-
-		// Also set internal index so spawn logic uses ON_GROUND
 		if (m_PlacementDropDown)
 		{
 			m_PlacementDropDown.SetIndex(1);
