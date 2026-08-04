df94d8a5d8f2d5ffef034a377d99e2de406693a6
Z User <z@container>
2026-08-04 14:24:24 +0000

    fix: replace non-existent CMD_ACTIONMOD_INTERACT with CMD_ACTIONMOD_ATTACHITEM
    - Changed ActionSingleUseBase to ActionInteractBase (matches ZenTerritories pattern)
    - Changed CMD_ACTIONMOD_INTERACT to CMD_ACTIONMOD_ATTACHITEM (INTERACT doesn't exist in DayZ API)
    - Changed Start() to OnExecuteServer() (correct for ActionInteractBase)

--- TerritoryFlags/4_World/TerritoryFlags/DZM_MenuAction.c
+++ TerritoryFlags/4_World/TerritoryFlags/DZM_MenuAction.c
@@ -1,42 +1,43 @@
 //------------------------------------------------------------------------------------------------------------------
-// Action для открытия меню территории при взгляде на флаг
-//------------------------------------------------------------------------------------------------------------------
+// Action для открытия меню территории при взгляде на флаг//------------------------------------------------------------------------------------------------------------------
 
-class ActionDZM_OpenMenu : ActionSingleUseBase
+class ActionDZM_OpenMenu extends ActionInteractBase
 {
-	void ActionDZM_OpenMenu() { m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_INTERACT; }
+        void ActionDZM_OpenMenu()
+        {
+                m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_ATTACHITEM;
+        }
 
-	override void CreateConditionComponents()
-	{
-		m_ConditionItem = new CCNonExistent;
-		m_ConditionTarget = new CCTCursor(UAMaxDistances.DEFAULT);
-	}
+        override void CreateConditionComponents()
+        {
+                m_ConditionItem = new CCNonExistent;
+                m_ConditionTarget = new CCTCursor(UAMaxDistances.DEFAULT);
+        }
 
-	override string GetText()
-	{
-		return "Меню территории";
-	}
+        override string GetText()
+        {
+                return "Меню территории";
+        }
 
-	override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item)
-	{
-		TerritoryFlag flag;
-		flag = TerritoryFlag.Cast(target.GetObject());
-		if (!flag) return false;
-		return true;
-	}
+        override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item)
+        {
+                TerritoryFlag flag;
+                flag = TerritoryFlag.Cast(target.GetObject());
+                if (!flag) return false;
+                return true;
+        }
 
-	override void Start(ActionData action_data)
-	{
-		super.Start(action_data);
-		TerritoryFlag flag;
-		string netID;
-		PlayerBase pb;
+        override void OnExecuteServer(ActionData action_data)
+        {
+                TerritoryFlag flag;
+                string netID;
+                PlayerBase pb;
 
-		flag = TerritoryFlag.Cast(action_data.m_Target.GetObject());
-		pb = PlayerBase.Cast(action_data.m_Player);
-		if (!flag || !pb || !pb.GetIdentity()) return;
+                flag = TerritoryFlag.Cast(action_data.m_Target.GetObject());
+                pb = PlayerBase.Cast(action_data.m_Player);
+                if (!flag || !pb || !pb.GetIdentity()) return;
 
-		netID = flag.GetNetworkID().ToString();
-		GetRPCManager().SendRPC("DZM_TF", "RequestOpenMenu", new Param1<string>(netID), true, pb.GetIdentity());
-	}
+                netID = flag.GetNetworkID().ToString();
+                GetRPCManager().SendRPC("DZM_TF", "RequestOpenMenu", new Param1<string>(netID), true, pb.GetIdentity());
+        }
 };
