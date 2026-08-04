3f1f7e943e1435596746b0b3f55d047a1c2e8107
Z User <z@container>
2026-08-04 15:57:39 +0000

    fix: BaseBuildingBase fallback for action registration + diagnostics

--- TerritoryFlags/4_World/TerritoryFlags/DZM_FlagExtension.c
+++ TerritoryFlags/4_World/TerritoryFlags/DZM_FlagExtension.c
@@ -1,69 +1,93 @@
 //--------------------------------------------------------------------------------------------------------------
-// DZM Flag Extension
+// DZM Flag Extension — добавляет UUID и экшен меню к флагштокам
 //--------------------------------------------------------------------------------------------------------------
 
 modded class TerritoryFlag
 {
-        protected string m_DZM_UUID = "";
+	protected string m_DZM_UUID = "";
 
-        string DZM_GetUUID()
-        {
-                return m_DZM_UUID;
-        }
+	void TerritoryFlag()
+	{
+		Print("[DZM_TerritoryFlags] TerritoryFlag constructor called");
+	}
 
-        override void OnStoreSave(ParamsWriteContext ctx)
-        {
-                super.OnStoreSave(ctx);
-                ctx.Write(m_DZM_UUID);
-        }
+	string DZM_GetUUID()
+	{
+		return m_DZM_UUID;
+	}
 
-        override bool OnStoreLoad(ParamsReadContext ctx, int version)
-        {
-                string savedUUID;
-                if (!super.OnStoreLoad(ctx, version)) return false;
-                savedUUID = "";
-                if (ctx.Read(savedUUID))
-                {
-                        m_DZM_UUID = savedUUID;
-                }
-                if (GetGame().IsServer() && m_DZM_UUID.Length() > 0)
-                {
-                        DZM_TerritoryManager.Get().RegisterFlag(m_DZM_UUID, GetPosition());
-                }
-                return true;
-        }
+	override void OnStoreSave(ParamsWriteContext ctx)
+	{
+		super.OnStoreSave(ctx);
+		ctx.Write(m_DZM_UUID);
+	}
 
-        override void EEInit()
-        {
-                super.EEInit();
-                if (GetGame().IsServer())
-                {
-                        if (m_DZM_UUID.Length() == 0)
-                        {
-                                m_DZM_UUID = DZM_TerritoryManager.GenerateUUID();
-                        }
-                        DZM_TerritoryManager.Get().RegisterFlag(m_DZM_UUID, GetPosition());
-                }
-        }
+	override bool OnStoreLoad(ParamsReadContext ctx, int version)
+	{
+		string savedUUID;
+		if (!super.OnStoreLoad(ctx, version)) return false;
+		savedUUID = "";
+		if (ctx.Read(savedUUID))
+		{
+			m_DZM_UUID = savedUUID;
+		}
+		if (GetGame().IsServer() && m_DZM_UUID.Length() > 0)
+		{
+			DZM_TerritoryManager.Get().RegisterFlag(m_DZM_UUID, GetPosition());
+		}
+		return true;
+	}
 
-        override void EEKilled(Object killer)
-        {
-                super.EEKilled(killer);
-                DZM_Cleanup();
-        }
+	override void EEInit()
+	{
+		super.EEInit();
+		Print("[DZM_TerritoryFlags] EEInit called for TerritoryFlag at " + GetPosition().ToString());
+		if (GetGame().IsServer())
+		{
+			if (m_DZM_UUID.Length() == 0)
+			{
+				m_DZM_UUID = DZM_TerritoryManager.GenerateUUID();
+			}
+			DZM_TerritoryManager.Get().RegisterFlag(m_DZM_UUID, GetPosition());
+		}
+	}
 
-        void DZM_Cleanup()
-        {
-                if (GetGame().IsServer())
-                {
-                        DZM_TerritoryManager.Get().UnregisterFlag(m_DZM_UUID);
-                }
-        }
+	override void EEKilled(Object killer)
+	{
+		super.EEKilled(killer);
+		DZM_Cleanup();
+	}
 
-        override void SetActions()
-        {
-                super.SetActions();
-        AddAction(ActionDZM_OpenMenu);
-                Print("[DZM_TerritoryFlags] SetActions - ActionDZM_OpenMenu added");
-        }
+	void DZM_Cleanup()
+	{
+		if (GetGame().IsServer())
+		{
+			DZM_TerritoryManager.Get().UnregisterFlag(m_DZM_UUID);
+		}
+	}
+
+	override void SetActions()
+	{
+		super.SetActions();
+		AddAction(ActionDZM_OpenMenu);
+		Print("[DZM_TerritoryFlags] SetActions - ActionDZM_OpenMenu added to TerritoryFlag");
+	}
+};
+
+//--------------------------------------------------------------------------------------------------------------
+// Fallback: добавляем экшен через BaseBuildingBase на случай если другой мод
+// перезаписывает TerritoryFlag.SetActions() без вызова super
+//--------------------------------------------------------------------------------------------------------------
+
+modded class BaseBuildingBase
+{
+	override void SetActions()
+	{
+		super.SetActions();
+		if (IsInherited(TerritoryFlag))
+		{
+			AddAction(ActionDZM_OpenMenu);
+			Print("[DZM_TerritoryFlags] SetActions - ActionDZM_OpenMenu added via BaseBuildingBase fallback");
+		}
+	}
 };
--- TerritoryFlags/4_World/TerritoryFlags/DZM_MenuAction.c
+++ TerritoryFlags/4_World/TerritoryFlags/DZM_MenuAction.c
@@ -4,35 +4,35 @@
 
 class ActionDZM_OpenMenu extends ActionInteractBase
 {
-        void ActionDZM_OpenMenu()
-        {
-                m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_ATTACHITEM;
-        }
+	void ActionDZM_OpenMenu()
+	{
+		m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_ATTACHITEM;
+		Print("[DZM_TerritoryFlags] ActionDZM_OpenMenu constructor called");
+	}
 
-        override string GetText()
-        {
-                return "Меню территории";
-        }
+	override string GetText()
+	{
+		return "Меню территории";
+	}
 
-        override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item)
-        {
-                TerritoryFlag flag;
-                flag = TerritoryFlag.Cast(target.GetObject());
-                if (!flag) return false;
-                Print("[DZM] ActionCondition OK, flag found");
-                return true;
-        }
+	override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item)
+	{
+		TerritoryFlag flag;
+		flag = TerritoryFlag.Cast(target.GetObject());
+		if (!flag) return false;
+		return true;
+	}
 
-        override void OnExecuteServer(ActionData action_data)
-        {
-                TerritoryFlag flag;
-                PlayerBase pb;
-                string uuid;
-                flag = TerritoryFlag.Cast(action_data.m_Target.GetObject());
-                pb = PlayerBase.Cast(action_data.m_Player);
-                if (!flag || !pb || !pb.GetIdentity()) return;
-                uuid = flag.DZM_GetUUID();
-                if (uuid.Length() == 0) return;
-                GetRPCManager().SendRPC("DZM_TF", "RequestOpenMenu", new Param1<string>(uuid), true, pb.GetIdentity());
-        }
+	override void OnExecuteServer(ActionData action_data)
+	{
+		TerritoryFlag flag;
+		PlayerBase pb;
+		string uuid;
+		flag = TerritoryFlag.Cast(action_data.m_Target.GetObject());
+		pb = PlayerBase.Cast(action_data.m_Player);
+		if (!flag || !pb || !pb.GetIdentity()) return;
+		uuid = flag.DZM_GetUUID();
+		if (uuid.Length() == 0) return;
+		GetRPCManager().SendRPC("DZM_TF", "RequestOpenMenu", new Param1<string>(uuid), true, pb.GetIdentity());
+	}
 };
